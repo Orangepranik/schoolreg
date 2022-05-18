3f3f1554d213fc2aa7ee4610a9c1d0e00ed30f1a
valolikx <bundakilya@gmail.com>
2022-05-18 22:54:29 +0300

    ff

---
+++ keyboards/keyboards.py
@@ -0,0 +1,11 @@
+from importi.importi import *
+def start_keyboard():
+    static_info_keyboard = types.ReplyKeyboardMarkup()
+    buttons = ['Подати заявку на вступ', 'Інформація про школу', 'Профіль']
+    static_info_keyboard.add(*buttons)
+    return static_info_keyboard
+def returntomenu():
+    statick_keyboard = types.ReplyKeyboardMarkup()
+    buttons = ['Повернутися назад']
+    statick_keyboard.add(*buttons)
+    return statick_keyboard
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import types
+
 from databasesqlite3.database import *
 from importi.importi import *
+from keyboards.keyboards import *
 API_TOKEN = "5167652549:AAGyl0koDoRjeBlZPtQd2GFQnVEYGngHB0Q"
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
@@ -16,15 +19,34 @@ async def welcome_user(message: types.Message):
     biouser = await bot.get_chat(iduser)
     biouserget = biouser.bio
     datainvite = time
-    checkuserid = cur.execute(f"SELECT * FROM users WHERE userid==('{iduser}')").fetchone()
+    checkuserid = cur.execute(f"SELECT * FROM users WHERE userid = ?",(iduser,)).fetchone()
     if checkuserid is None:
         cur.execute("INSERT INTO users(username, userid, userbio, timestart) VALUES(?, ?, ?, ?)", (username, iduser, biouserget, datainvite))
     else:
         pass
     base.commit()
-    await message.answer("Вітаємо вас,")
+    await message.answer("Вітаємо вас!\n В цьому боті ви можете взнати актуальну інформацію про шкільний заклад під назвою 'ЛУЦЬКИЙ НВК №26'\n Щоб дізнатися інформацію про школу введіть або нажміть кнопку 'Інформація про школу'",
+                         reply_markup = start_keyboard())
 
-# base.execute("CREATE TABLE IF NOT EXISTS users(username VARCHAR(256),userid VARCHAR(300),phonenumber VARCHAR(30), userbio VARCHAR(150),timestart VARCHAR(100), timerequest VARCHAR(100)")
-# base.commit()
+
+@dp.message_handler(Text(equals = 'Інформація про школу'))
+async def infoforshool(message: types.Message):
+    await message.answer()
+
+
+@dp.message_handler(Text(equals = 'Профіль'))
+async def give_profile(message: types.Message):
+    info_give_profile = cur.execute("SELECT * FROM users WHERE userid=(?)", (message.from_user.id,)).fetchone()
+    await message.reply(f"Ваше ім`я в телеграммі: {info_give_profile[0]}\n Дата коли ви звернулися перший раз до цього бота: {info_give_profile[4]}", reply_markup = returntomenu())
+
+
+@dp.message_handler(Text(equals = 'Повернутися назад'))
+async def returnback(message: types.Message):
+    await message.answer("Добре",reply_markup = start_keyboard())
+
+
+@dp
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
